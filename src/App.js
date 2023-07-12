1690e27fd749c085528d41c5a5c123cbd99179ae
ShreyasMP17 <shreyasmp.7@gmail.com>
2023-07-12 20:20:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login/>}/>
+      <Route path="/board-/" element={<Login/>}/>
       <Route path="/dashboard" element={<Home/>}/>
     </Routes>
     </BrowserRouter>
